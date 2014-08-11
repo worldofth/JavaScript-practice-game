545b2abccf6b83005912f0b4bad36b560003ed91
worldofth <worldofth@hotmail.co.uk>
2014-08-12 00:06:35 +0100

    game loop

--- app/main.js
+++ app/main.js
@@ -30,23 +30,70 @@
 			ctx.drawImage(buffer, 0, 0);
 		};
 
+		function getbctx(){
+			return bctx;
+		}
+
 		return {
 			"setup": setup,
 			"clear": clear,
 			"draw": draw,
-			"ctx": bctx
+			"ctx": getbctx
 		};
 	}());
 
-	canvas.setup(200,200, 'canvas');
+	var game = {
+		fps: 60,
+		x: 1,
+		preupdate: function(){},
 
-	function draw(){
-		canvas.clear();
-		canvas.draw();
+		postupdate:function(){},
+
+		update: function(){
+			this.preupdate();
+
+			//update
+			this.x+=5;
+
+			this.postupdate();
+		},
+
+		draw: function(ctx){
+			ctx.fillStyle = "#831616";
+			ctx.beginPath();
+			ctx.rect(this.x, 25, 50, 50);
+			ctx.closePath();
+			ctx.fill();
+		},
+
+		run:(function(){
+			var loops = 0,
+				skipTicks = 1000/ this.fps,
+				maxFrameSkip = 10,
+				nextGameTick = (new Date).getTime();
+			return function(){
+				loops = 0;
+
+				while((new Date).getTime() > nextGameTick && loops < maxFrameSkip){
+					game.update();
+					nextGameTick += skipTicks;
+					loops++;
+				}
+				canvas.clear();
+				this.draw(canvas.ctx());
+				canvas.draw();
+			};
+		}())
+	};
+
+	function init(){
+		canvas.setup(200,200, 'canvas');
 	}
 
+
 	window.onload = function(){
-		draw();
+		init();
+		game.intervalId = setInterval(game.run(), 1000);
 	}
 
 }());
