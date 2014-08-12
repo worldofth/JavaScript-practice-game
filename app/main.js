5f5ff4d17390af6451ffa5d6639fb3cae3134ba1
worldofth <worldofth@hotmail.co.uk>
2014-08-12 11:53:10 +0100

    update loop with webkit frame animation

--- app/main.js
+++ app/main.js
@@ -1,101 +1,128 @@
 (function(){
-	var canvas = (function(){
-		var width, height, can, ctx, buffer, bctx;
-
-		function setup(w,h,c){
-			width = w;
-			height = h;
-			can = document.getElementById(c);
-
-			ctx = can.getContext('2d');
-			can.width = width;
-			can.height = height;
-
-			buffer = document.createElement('canvas');
-			bctx = buffer.getContext('2d');
-			buffer.width = width;
-			buffer.height = height;
-		};
-
-		function clear(clr){
-			if(!clr) clr = "#efefef";
-			bctx.fillStyle = clr;
-			bctx.beginPath();
-			bctx.rect(0, 0, width, height);
-			bctx.closePath();
-			bctx.fill();
-		};
-
-		function draw(){
-			ctx.drawImage(buffer, 0, 0);
-		};
-
-		function getbctx(){
-			return bctx;
+	(function(){
+		var onEachFrame;
+		if(window.webkitRequestAnimationFrame){
+			onEachFrame = function(cb){
+				var _cb = function(){ cb(); window.webkitRequestAnimationFrame(_cb); };
+				_cb();
+			}
+		}else if(window.mozRequestAnimationFrame){
+			onEachFrame = function(cb){
+				var _cb = function(){ cb(); window.mozRequestAnimationFrame(_cb); };
+				_cb();
+			}
+		}else{
+			onEachFrame = function(cb){
+				setInterval(cb, 0);
+			}
 		}
 
-		return {
-			"setup": setup,
-			"clear": clear,
-			"draw": draw,
-			"ctx": getbctx
-		};
+		window.onEachFrame = onEachFrame;
 	}());
 
-	var game = {
-		_intervalId: 0,
-		fps: 60,
-		x: 1,
-		preupdate: function(){},
+	var game = (function(){
+		var canvas = (function(){
+			var width, height, can, ctx, buffer, bctx;
 
-		postupdate:function(){},
+			function setup(w,h,c){
+				width = w;
+				height = h;
+				can = document.getElementById(c);
 
-		update: function(){
-			this.preupdate();
+				ctx = can.getContext('2d');
+				can.width = width;
+				can.height = height;
 
+				buffer = document.createElement('canvas');
+				bctx = buffer.getContext('2d');
+				buffer.width = width;
+				buffer.height = height;
+			};
+
+			function clear(clr){
+				if(!clr) clr = "#efefef";
+				bctx.fillStyle = clr;
+				bctx.beginPath();
+				bctx.rect(0, 0, width, height);
+				bctx.closePath();
+				bctx.fill();
+			};
+
+			function draw(){
+				ctx.drawImage(buffer, 0, 0);
+			};
+
+			return {
+				"setup": setup,
+				"clear": clear,
+				"draw": draw,
+				"ctx": function(){return bctx;}
+			};
+		}());
+
+
+		var x = 1;
+
+		var init = function(){
+			canvas.setup(200,200, 'canvas');
+		};
+
+		var preupdate = function(){};
+		var postupdate = function(){};
+
+		var update = function(){
+			preupdate();
 			//update
-			this.x+=5;
+			x+=1;
 
-			this.postupdate();
-		},
+			postupdate();
+		};
 
-		draw: function(){
+
+		var draw = function(){
 			canvas.ctx().fillStyle = "#831616";
 			canvas.ctx().beginPath();
-			canvas.ctx().rect(this.x, 25, 50, 50);
+			canvas.ctx().rect(game.x, 25, 50, 50);
 			canvas.ctx().closePath();
 			canvas.ctx().fill();
-		},
+		};
 
-		run:(function(){
-			console.log("1");
-			var loops = 0,
-				skipTicks = 1000/ this.fps,
-				maxFrameSkip = 10,
-				nextGameTick = (new Date).getTime();
+		var run = (function(){
+			var loops = 0;
+			var fps = 60;
+			var skipTicks = (1000 / fps);
+			var maxFrameSkip = 10;
+			var nextGameTick = (new Date).getTime();
+
 			return function(){
 				loops = 0;
-
 				while((new Date).getTime() > nextGameTick && loops < maxFrameSkip){
-					game.update();
+					update();
 					nextGameTick += skipTicks;
 					loops++;
 				}
+
 				canvas.clear();
-				this.draw();
+				draw();
 				canvas.draw();
 			};
-		}())
-	};
+		}());
+
+		return {
+			"run": run,
+			"init": init
+		};
+	}());
 
 	function init(){
-		canvas.setup(200,200, 'canvas');
+		game.init();
 	}
 
 
 	window.onload = function(){
 		init();
-		game._intervalId = setInterval(game.run(), 0);
+		game.run();
+		//window.onEachFrame(game.run);
 	}
 
 }());
