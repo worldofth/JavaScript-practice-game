d52fac54fe895026f994ef8cbcb2c09c22180f97
worldofth <worldofth@hotmail.co.uk>
2014-09-10 22:19:54 +0100

    updated comment style on util.js

--- app/modules/util.js
+++ app/modules/util.js
@@ -1,18 +1,21 @@
+/* globals window */
+
 /**
-* @author Tom Hopkins https://github.com/worldofth
+* @author Tom Hopkins [https://github.com/worldofth]
+* @file Small Utitlty Objects
+* @version 1.0.0
 */
 
 /**
 * engine namespace
+* @namespace testgame
 */
 var testgame = testgame || {};
 
-/* globals window */
-
 /**
-* @class Anon
-* @classdesc sets up the request animation method if the browser has it
+* sets up the request animation method if the browser has it
 * if not it falls back to set interval
+* @module Sets up timer function
 */
 (function(){
 	'use strict';
@@ -42,9 +45,8 @@ var testgame = testgame || {};
 }());
 
 /**
-* @class testgame.util
-* @classdesc a closure namespace for util objects
-* @static
+* a closure namespace for util objects
+* @module testgame.util
 */
 (function(testgame){
     'use strict';
@@ -52,20 +54,20 @@ var testgame = testgame || {};
     /**
 	* @class testgame.util.Vec2
 	* @classdesc holds the position and velocity of an object
-	* @constructor
+	* @constructs Vec2
 	* @param { number } x - starting x position
 	* @param { number } y - starting y position
 	*/
-	var Vec2 = function(x,y){
+	var Vec2 = function(x, y){
 		/**
 		* @property { number } - sets the intial x position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.x = x || 0;
 
 		/**
 		* @property { number } - sets the intial y position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.y = y || 0;
 
@@ -80,31 +82,33 @@ var testgame = testgame || {};
 		this.vy = 0;
 	};
 
-	//constructor
 	Vec2.prototype.constructor = Vec2;
-
 
 	Vec2.prototype = (function(){
         /**
         * updates the x and y position based on the velocities
         *
         * @method testgame.util.Vec2#update
+        * @public
         */
         var update = function(){
             this.x += this.vx;
             this.y += this.vy;
         },
-
         /**
         * clones a Vec2, returning a new copy of this object
         *
         * @method testgame.util.Vec2#clone
-        * @return { Object } - returns a copy of this Vec2
+        * @public
+        * @returns { Object } - returns a copy of this Vec2
         */
         clone = function(){
             return new Vec2(this.x, this.y);
         };
 
+        /**
+        * @exports public functions
+        */
         return{
             update: update,
             clone: clone
@@ -112,25 +116,24 @@ var testgame = testgame || {};
 
     }());
 
-
 	/**
 	* @class testgame.util.Point
 	* @classdesc holds an x,y point
-	* @constructor
+	* @constructs Point
 	* @param { number } x - starting x position
 	* @param { number } y - starting y position
 	*/
-	var Point = function(x,y){
+	var Point = function(x, y){
 
 		/**
 		* @property { number } - sets the intial x position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.x = x || 0;
 
 		/**
 		* @property { number } - sets the intial y position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.y = y || 0;
 	};
@@ -138,76 +141,76 @@ var testgame = testgame || {};
 	/**
 	* @class testgame.util.Gradient
 	* @classdesc a Gradient object to hold a colour Gradient
-	* @constructor
+	* @constructs Gradient
 	* @param { number } x - starting x position
 	* @param { number } y - starting y position
 	* @param { number } x1 - starting x1 position
 	* @param { number } y1 - starting y1 position
 	* @param { number } r - starting radius position
 	* @param { number } r1 - starting radius1 position
-	* @param { string/Object } colour - starting colour as a string or a grad object
+	* @param { string|Array<string> } colour - starting colour as a string or a grad object
 	*/
-	var Gradient = function(x,y,x1,y1,r,r1,colours){
+	var Gradient = function(x, y, x1, y1, r, r1, colours){
 		/**
 		* if the radius is truthy or greater than 0 then this is a radial gradient
 		*
-		* @property { string } - sets if the gradient is radial or not
+		* @property { boolean } - sets if the gradient is radial or not
 		*/
 		this.isRadial = !!r || r>0;
 
 		/**
 		* @property { number } - sets the intial radius
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.r = r || 0;
 
 		/**
 		* @property { number } - sets the intial radius1
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.r1 = r1 || 0;
 
 		/**
 		* @property { number } - sets the intial x position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.x = x || 0;
 
 		/**
 		* @property { number } - sets the intial y position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.y = y || 0;
 
 		/**
 		* @property { number } - sets the intial x1 position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.x1 = x1 || 0;
 
 		/**
 		* @property { number } - sets the intial x2 position
-		* @default 0 - defaults to 0 if nothing is passed
+		* @default [0] - defaults to 0 if nothing is passed
 		*/
 		this.y1 = y1 || 0;
 
 		/**
-		* @property { Array } - sets the colours, which is an array of position (0-1) and colour strings
-		* @default [] - if empty defaults to an array
+		* @property { Array<string> } - sets the colours, which is an array of position (0-1) and colour strings
+		* @default [[]] - if empty defaults to an array
 		*/
 		this.colours = colours || [];
 	};
 
-	//constructor
-	Gradient.prototype.constructor = Gradient;
 
+	Gradient.prototype.constructor = Gradient;
 
 	Gradient.prototype = (function(){
         /**
         * clones a Gradient object, returning a new copy of this object
         *
         * @method testgame.util.Gradient#clone
-        * @return { Object } - returns a copy of this Gradient
+        * @public
+        * @returns { Gradient } - returns a copy of this Gradient
         */
         var clone = function(){
             return new Gradient(this.x, this.y, this.x1, this.y1, this.r, this.r1, this.colours);
@@ -218,10 +221,10 @@ var testgame = testgame || {};
         * string colour, hex or rgb
         *
         * @method testgame.util.Gradient#addColour
+        * @public
         * @param { number } pos - a number between 0 - 1 pertaining to the colour's position in the gradient
         * @param { string } colour - either a hex string to a rgb
         * @chainable
-        * @return { Object } - returns this for chain calls
         */
         addColour = function(pos, colour){
             pos = pos > 1 ? 1 : pos;
@@ -229,6 +232,9 @@ var testgame = testgame || {};
             return this;
         };
 
+        /**
+        * @exports public function
+        */
         return {
             clone: clone,
             addColour: addColour
@@ -237,13 +243,12 @@ var testgame = testgame || {};
     }());
 
     /**
-	* defines the public interface to the util object, all other methods
-	* are private inside the util closure.
-	*
-	* @return { Object } - public interface for the util object
-	*/
+    * @exports public objects/functions
+    */
 	testgame.util = {
-		//graphic object types
+		/**
+        * @enum shapes types
+        */
 		"TYPE_REC" 	: "Rectangle",
 		"TYPE_CIR" 	: "Circle",
 		"TYPE_RREC" : "RoundRectangle",
